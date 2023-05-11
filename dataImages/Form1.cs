c2182f245d0a1f6f519615b0ada904f81cdf92d9
Carlos Perez <per.ckarlos@gmail.com>
2023-05-11 15:53:39 -0500

    practicing binary reader library

--- dataImages/Form1.cs
+++ dataImages/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Security.Cryptography;
+
 namespace dataImages
 {
     public partial class Form1 : Form
@@ -12,23 +15,30 @@ namespace dataImages
         private void btnOpen_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == DialogResult.OK)
+            if(ofd.ShowDialog() == DialogResult.OK )
             {
-                btnRead.Enabled = true;
+                txtFile.Text = ofd.FileName;
                 path = ofd.FileName;
-                txtFile.Text = path;
             }
         }
 
         private void btnRead_Click(object sender, EventArgs e)
         {
-            BinaryReader br = new BinaryReader(File.OpenRead(path));
-            //txtData.Text = br.ReadChar().ToString();
-            //foreach (char myChar in br.ReadChars(6)) txtData.Text += myChar;
+            FileStream fs = new FileStream(path, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            foreach (char c in br.ReadChars(5)) txtData.Text += c;
+
+            txtData.Text += "\r\n";
+            
             br.BaseStream.Position = 0x00;
-            byte[] buffer = br.ReadBytes(12);
-            Array.Reverse(buffer);
-            txtData.Text=BitConverter.ToInt32(buffer, 0).ToString("x");
+            Byte[] bytes = br.ReadBytes(6);
+            MessageBox.Show(bytes.Length+"");
+            foreach (char c in bytes) txtData.Text += c;
+            txtData.Text += "\r\n";
+            Array.Reverse(bytes);
+            foreach (char c in br.ReadChars(5)) txtData.Text += c;
+            txtData.Text += "\r\n";
+            //txtData.Text += BitConverter.ToInt64(bytes, 0).ToString("x");
             br.Dispose();
         }
     }
